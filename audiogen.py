6976cf58f1d95fa85f0cb1444aea241162daddb5
Mike Heavers <mheavers@gmail.com>
2023-10-12 14:56:08 -0700

    replace audiocraft with master, including new audiogen.py script for a sample audio generation

---
+++ audiogen.py
@@ -0,0 +1,13 @@
+import torchaudio
+from audiocraft.models import AudioGen
+from audiocraft.data.audio import audio_write
+
+model = AudioGen.get_pretrained('facebook/audiogen-medium')
+model.set_generation_params(duration=5)  # generate [duration] seconds.
+descriptions = ['dog barking', 'sirene of an emergency vehicle']
+wav = model.generate(descriptions)  # generates samples for all descriptions in array.
+
+for idx, one_wav in enumerate(wav):
+    # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+    audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
+    print(f'Generated {idx}th sample.')
